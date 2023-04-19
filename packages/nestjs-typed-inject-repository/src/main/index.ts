d6b7e5d8badc3a3ce24853aef4d8f48b036ea308
Tamas Hegedus <sorgloomer@gmail.com>
2023-04-19 04:13:46 +0200

    fix: merged nestjs-typed-inject and nestjs-typed-inject-repository

--- packages/nestjs-typed-inject-repository/src/main/index.ts
+++
@@ -1,7 +0,0 @@
-import { InjectRepository } from '@nestjs/typeorm';
-import { ConstructorFor, TypedParameterDecorator } from 'typesafe-decorators';
-import { ObjectLiteral, Repository } from 'typeorm';
-
-export const TypedInjectRepository: <
-  Entity extends ObjectLiteral
->(entity: ConstructorFor<Entity>) => TypedParameterDecorator<Repository<Entity>> = InjectRepository;
