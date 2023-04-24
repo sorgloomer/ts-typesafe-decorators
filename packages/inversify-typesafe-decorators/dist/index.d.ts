ff12e27f4e2a73f6c9e2d2d649eb5b683769326f
Tamas Hegedus <sorgloomer@gmail.com>
2023-04-24 23:21:26 +0200

    fix: fixed wrong dist folder

--- packages/inversify-typesafe-decorators/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { inject } from 'inversify';
-import { TypedParameterDecorator } from 'typesafe-decorators';
-export declare const TypedInject: <T>(...args: Parameters<typeof inject<T>>) => TypedParameterDecorator<T>;
--- packages/inversify-typesafe-decorators/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TypedInject = void 0;
-var inversify_1 = require("inversify");
-exports.TypedInject = inversify_1.inject;
